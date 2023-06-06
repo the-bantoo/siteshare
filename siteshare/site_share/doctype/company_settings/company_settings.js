d691b12f8bb753cea22d700ed607f04c9d850b82
Adam <adam.daveed@gmail.com>
2023-06-06 16:02:26 +0200

    company settings and workspace

---
+++ siteshare/site_share/doctype/company_settings/company_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Bantoo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Company Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ siteshare/site_share/doctype/company_settings/test_company_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Bantoo and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCompanySettings(FrappeTestCase):
+	pass
